df26491304c3f4bc516116f41357306fb0145df4
ent1k1337@yandex.ru <ent1k1337@yandex.ru>
2022-09-08 17:58:59 +0300

    changed name Boss to BossStateBahavior

---
+++ Assets/Resources/Scripts/Boss/BossStateBehavior.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Resources.Scripts.Boss.States;
+using UnityEngine;
+using Random = System.Random;
+
+namespace Resources.Scripts.Boss
+{
+    [RequireComponent(typeof(Animator))]
+    public class BossStateBehavior : MonoBehaviour, IStationStateSwitcher
+    {
+        [SerializeField] private Player.Player _player;
+        [SerializeField, Range(0.1f, 10f)] private float _timeBetweenStates;
+        
+        [Header("Fist Attack")]
+        [SerializeField] private float _fistAttackDistance;
+        
+        [Header("AOE Attack")]
+        [SerializeField] private SpriteRenderer _redCircle;
+        [SerializeField] private ParticleSystem _stripesEffect;
+        [SerializeField] private ParticleSystem _smokeExplosionEffect;
+        
+        private Animator _animator;
+        private bool _isReadyChangeState = true;
+        private bool _isSuperAttack = false;
+        private float _currentTimeBetweenStates;
+
+        private State _startState;
+        private State _currentState;
+        private List<State> _allStates;
+        private List<State> _superAttacks;
+
+        private bool _isArmsLength => GetDistanceToPlayer() <= _fistAttackDistance;
+
+        private float GetDistanceToPlayer() => Vector3.Distance(transform.position, _player.transform.position);
+        
+        private void StatesInitialize()
+        {
+            _allStates = new List<State>
+            {
+                new IdleState(_animator, this),
+                new TiredState(_animator, this),
+                new FistAttackState(_animator,this),
+                new DirectedAttackState(_animator,this),
+                new AoeAttackState(_animator,this, this, _redCircle, _stripesEffect, _smokeExplosionEffect),
+            };
+            _superAttacks = new List<State> {_allStates[4]};
+            _currentState = _allStates.FirstOrDefault(s => s is IdleState);
+            _currentState.Enter();
+        }
+        
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+        }
+
+        private void Start()
+        {
+            StatesInitialize();
+        }
+
+        public void AnimationEvent(int index)
+        {
+            _currentState.AnimationEvent(index);
+        }
+        
+        private void Update()
+        {
+            Debug.Log($"{_isArmsLength}");
+            if (_isSuperAttack)
+                return;
+            
+            if (_isReadyChangeState)
+                _currentTimeBetweenStates += Time.deltaTime;
+            
+            if (_isArmsLength && _currentState is IdleState)
+                SwitchState<FistAttackState>();
+            else if (_currentTimeBetweenStates >= _timeBetweenStates && !_isArmsLength)
+                RandomChoiceSuperAttacks();
+            else if (_currentTimeBetweenStates < _timeBetweenStates && _isArmsLength)
+                SwitchState<IdleState>();
+        }
+
+        private void RandomChoiceSuperAttacks()
+        {
+            _isSuperAttack = true;
+            var random = new Random();
+            var randomIndex = random.Next(0, _superAttacks.Count);
+            _currentState.Exit();
+            _currentState = _superAttacks[randomIndex];
+            _currentState.Enter();
+        }
+
+        public void SwitchState<T>() where T : State
+        {
+            _currentState.Exit();
+            _currentState = _allStates.FirstOrDefault(s => s is T);
+            _currentState.Enter();
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/Boss/States/TiredState.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UniRx;
+
+namespace Resources.Scripts.Boss.States
+{
+    public class TiredState : State
+    {
+        private CompositeDisposable _disposable = new ();
+        private readonly int _restHash = Animator.StringToHash("Armature_BossTired");
+        private readonly float _timeTired = 1.5f;
+        
+        public TiredState(Animator animator, IStationStateSwitcher stateSwitcher) : base(animator, stateSwitcher)
+        {
+        }
+
+        public override void Enter()
+        {
+            Debug.Log("Вошел в состояние Tired");
+            
+            _animator.CrossFade(_restHash, 0.15f);
+
+            var currentTimeTired = 0f;
+            Observable.EveryUpdate().Subscribe(_ =>
+            {
+                currentTimeTired += Time.deltaTime;
+                if (currentTimeTired >= _timeTired)
+                {
+                    _disposable.Clear();
+                    _stateSwitcher.SwitchState<IdleState>();
+                }
+            }).AddTo(_disposable);
+        }
+
+        public override void Exit()
+        {
+            Debug.Log("Вышел из состояния Tired");
+        }
+    }
+}
